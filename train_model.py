480746ae4b3f6558102e094a727c94655c9a157d
Jackson Searle <jacksonsearle24@gmail.com>
2023-12-05 13:56:49 -0700

    better torchtext tutorial

--- train_model.py
+++ train_model.py
@@ -202,51 +202,13 @@ class TransformerModel(nn.Module):
         return F.softmax(token_logits, dim=-1)
 
 def load_dataset():
-    #! This function is dogwater, plz use a real tokenizer, this is just for testing
-    #! taken from this webpage: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-    # TODO: Use sentencepiece as a tokenizer instead (or something else that is also good)
-    train_iter = WikiText2(split='train')
-    tokenizer = get_tokenizer('basic_english')
-    vocab = build_vocab_from_iterator(map(tokenizer, train_iter), specials=['<unk>'])
-    vocab.set_default_index(vocab['<unk>'])
+    # This webpage shows a way to read torchtext into a Dataloader
+    # use their tokenizer for now to get training going
+    # : https://pytorch.org/tutorials/beginner/text_sentiment_ngrams_tutorial.html
+    # TODO: Use sentencepice tokenizer in the future
     
-    def data_process(raw_text_iter: dataset.IterableDataset) -> Tensor:
-        """Converts raw text into a flat Tensor."""
-        data = [torch.tensor(vocab(tokenizer(item)), dtype=torch.long) for item in raw_text_iter]
-        return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
-    
-    # ``train_iter`` was "consumed" by the process of building the vocab,
-    # so we have to create it again
-    train_iter, val_iter, test_iter = WikiText2()
-    train_data = data_process(train_iter)
-    val_data = data_process(val_iter)
-    test_data = data_process(test_iter)
-    
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    def batchify(data: Tensor, bsz: int) -> Tensor:
-        """Divides the data into ``bsz`` separate sequences, removing extra elements
-        that wouldn't cleanly fit.
-    
-        Arguments:
-            data: Tensor, shape ``[N]``
-            bsz: int, batch size
-    
-        Returns:
-            Tensor of shape ``[N // bsz, bsz]``
-        """
-        seq_len = data.size(0) // bsz
-        data = data[:seq_len * bsz]
-        data = data.view(bsz, seq_len).t().contiguous()
-        return data.to(device)
-    
-    batch_size = 20
-    eval_batch_size = 10
-    train_data = batchify(train_data, batch_size)  # shape ``[seq_len, batch_size]``
-    val_data = batchify(val_data, eval_batch_size)
-    test_data = batchify(test_data, eval_batch_size)
 
-    return batch_size, eval_batch_size, train_data, val_data, test_data
+    return None, None, None, None, None
 
 def model_info(model):
     # Print model summary
