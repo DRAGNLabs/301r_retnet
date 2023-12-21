d44b6a7d5e2a767ead060bb6eed0085293f48c4c
Nathan Prisbrey <ntpbanana@gmail.com>
2023-12-20 17:55:29 -0700

    Maintain Alphabetical Order Of Parser Arguments
    Also added "-b" as a short option to specify batch size.

--- train_model.py
+++ train_model.py
@@ -209,12 +209,18 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--activation-dropout", type=float, default=0.0,
             help="Probability of element to be zeroed in dropout layer " + \
                     "after activation between FFN layers.")
+    parser.add_argument("-b", "--batch-size", type=int, default=32,
+            help="Batch size.")
     parser.add_argument("-c", "--checkpoint-activations", action="store_true",
             default=False, help="Use checkpointing.")
+    parser.add_argument("--device", type=str, default="cuda",
+            help="Device to use (ex: 'cpu', 'cuda', or 'cuda:0').")
     parser.add_argument("-d", "--dropout", type=float, default=0.1,
             help="Probability of element to be zeroed in dropout layer.")
     parser.add_argument("-e", "--embed-dim", type=int, default=768,
             help="Embedding dimension size of each token.")
+    parser.add_argument("--epochs", type=int, default=10,
+            help="Number of epochs to train for.")
     parser.add_argument("-f", "--ffn-dim", type=int, default=1280,
             help="FFN hidden layer size.")
     parser.add_argument("--fsdp", action="store_true", default=False,
@@ -238,12 +244,6 @@ if __name__ == "__main__":
             help="Value embed dimension size.")
     parser.add_argument("--vocab-size", type=int, required=True,
             help="Maximum number of unique tokens in vocabulary.")
-    parser.add_argument("--batch-size", type=int, default=32,
-            help="Batch size.")
-    parser.add_argument("--device", type=str, default="cuda",
-            help="Device to use (ex: 'cpu', 'cuda', or 'cuda:0').")
-    parser.add_argument("--epochs", type=int, default=10,
-            help="Number of epochs to train for.")
 
     args = parser.parse_args()
 
