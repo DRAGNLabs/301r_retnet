cc78ceaf574165959eb25e50a24d4d090dbcfa45
Nathan Prisbrey <ntpbanana@gmail.com>
2024-01-10 09:14:06 +0000

    Update Example Scripts
    Include all possible arguments as examples of possible inputs. Reorder
    arguments to be in alphabetical order. Update values to be closer to
    what would be used in a data run.

--- scripts/retnet.sh
+++ scripts/retnet.sh
@@ -1,18 +1,23 @@
 python3 ../train_model.py \
-    --activation-dropout 0.0 \
-    --dropout 0.0 \
+    --activation-dropout 0.1 \
+    --batch-size 128 \
     --checkpoints \
-    --embed-dim 32 \
-    --ffn-dim 64 \
+    --dataset-feature text \
+    --dataset-name c4 \
+    --dataset-subset en \
+    --device cuda \
+    --dropout 0.1 \
+    --embed-dim 128 \
+    --epochs 10 \
+    --ffn-dim 1024 \
     --fsdp \
-    --layers 1 \
+    --layers 6 \
     --lr 0.001 \
     --model retnet \
     --heads 8 \
-    --seq-len 128 \
-    --value-embed-dim 32 \
-    --vocab-size 28783 \
-    --device cpu \
-    --epochs 10 \
-    --batch-size 128 \
     --rand-seed 42 \
+    --seq-len 128 \
+    --splits 0.7 0.2 0.1 \
+    --val-freq 3 \
+    --value-embed-dim 128 \
+    --vocab-size 100000 \
