ab772dc71d97ea4e8145ecb658d9b007a7aca642
Nathan Prisbrey <ntpbanana@gmail.com>
2024-01-18 06:45:18 +0000

    Add Docstring And Move Parser Out Of Method
    Added more documentation to download_data.py and moved the argument
    parser out of the function. This makes the function reusable in other
    contexts as desired in the future.

--- download_data.py
+++ download_data.py
@@ -4,19 +4,22 @@ from pathlib import Path
 
 REPO_ROOT_NAME = "301r_retnet"
 
-def main():
-    # Initialize, setup, and parse the argument parser
-    parser = ArgumentParser(prog="Data Downloader")
+def download_data(
+        dataset_name: str,
+        dataset_subset: str,
+        dataset_dir: str):
+    """ Download dataset from Hugging Face.
 
-    parser.add_argument("--dataset-name", type=str, default="wikitext",
-        help="Hugging Face dataset name. Should also set --dataset-subset.")
-    parser.add_argument("--dataset-subset", type=str, default="wikitext-2-v1",
-        help="Subset/config to use for Hugging Face dataset.")
-    parser.add_argument("--dataset-dir", type=str, default=None,
-        help="Directory to save dataset to.")
+    It is useful to download the dataset before trying to train the model when
+    the training will take place in a location without access to the internet.
 
-    args = parser.parse_args()
-
+    Args:
+        dataset_name (str): Name of Hugging Face dataset.
+        dataset_subset (str): Configuration/subset of dataset to use.
+        dataset_dir (str): Absolute path to directory in which to download the
+            dataset. If None, will default to the "data" directory at the root
+            of the repository.
+    """
     # Get path of repository root folder
     repo_root_dir = Path(__file__)
     while REPO_ROOT_NAME not in repo_root_dir.name:
@@ -36,4 +39,21 @@ def main():
         trust_remote_code=True)
 
 if __name__ == "__main__":
-    main()
+    # Initialize, setup, and parse the argument parser
+    parser = ArgumentParser(prog="Data Downloader")
+
+    parser.add_argument("--dataset-name", type=str, required=True,
+        default="wikitext",
+        help="Hugging Face dataset name. Should also set --dataset-subset.")
+    parser.add_argument("--dataset-subset", type=str, required=True,
+        default="wikitext-2-v1",
+        help="Subset/config to use for Hugging Face dataset.")
+    parser.add_argument("--dataset-dir", type=str, required=True, default=None,
+        help="Directory to save dataset to.")
+
+    args = parser.parse_args()
+
+    download_data(
+        dataset_name=args.dataset_name,
+        dataset_subset=args.dataset_subset,
+        dataset_dir=args.dataset_dir)
