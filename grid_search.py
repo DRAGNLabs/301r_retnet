fdc004d9690aa4e2337d8a7ca5d7fba5facb92f6
Kimball Jardine <kimball.jardine@outlook.com>
2024-01-19 09:34:41 -0700

    Clean up grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -17,8 +17,8 @@ learning_rates = [0.01, 0.001]
 # Cartesian product of all hyperparameters
 param_combinations = list(itertools.product(learning_rates, embed_dims, batch_sizes))
 
-def evaluate_models(model1, model2, retnet_loss, transformer_loss):
-    return abs(retnet_loss - transformer_loss)
+def evaluate_models(model1, model2, model1_loss, model2_loss):
+    return abs(model1_loss - model2_loss)
 
 # Open a CSV file to write the results
 with open('model_training_results.csv', 'w', newline='') as file:
@@ -33,25 +33,12 @@ with open('model_training_results.csv', 'w', newline='') as file:
         retnet_model, avg_loss_retnet = train_model(embed_dim=embed_dim, lr=lr, batch_size=batch_size, model_type="retnet")
         transformer_model, avg_loss_transformer = train_model(embed_dim=embed_dim, lr=lr, batch_size=batch_size, model_type="transformer")
 
-        # avg_loss_retnet = 1
-        # avg_loss_transformer = 2
-        # retnet_model = None
-        # transformer_model = None
-
         similarity_score = evaluate_models(retnet_model, transformer_model, avg_loss_retnet, avg_loss_transformer)
         
         training_time = time.time() - start_time
 
         writer.writerow([lr, embed_dim, batch_size, avg_loss_retnet, avg_loss_transformer, similarity_score, training_time])
         similarity_scores[(lr, embed_dim, batch_size)] = similarity_score
-
-        # # Sleep for 5 seconds
-        # time.sleep(5)
-        # counter += 1
-
-        # # Raise exception after 5 iterations
-        # if counter == 5:
-        #     raise Exception("Test exception")
 
     # Find the most and least similar parameters, just for fun
     most_similar_params = min(similarity_scores, key=similarity_scores.get)
