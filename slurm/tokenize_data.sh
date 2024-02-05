9c343621e46036abaf39ea940cf60f82fbaca67a
JayOrten <jay.orten@gmail.com>
2024-02-05 08:36:41 -0700

    add slurm scripts

---
+++ slurm/tokenize_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+
+#SBATCH --time=10:00:00   # walltime
+#SBATCH --ntasks-per-node=1# number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem=64G   # memory per CPU core
+#SBATCH -J "tokenize_data"   # job name
+#SBATCH --output=%x_%j.out
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+mamba activate retnet
+python3 ../../tokenize_data.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
+
---
+++ slurm/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash --login
+
+#SBATCH --time=10:00:00   # walltime
+#SBATCH --ntasks-per-node=1# number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:1
+#SBATCH --qos=cs
+#SBATCH --mem=64G   # memory per CPU core
+#SBATCH -J "train"   # job name
+#SBATCH --output=%x_%j.out
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+mamba activate retnet
+python3 ../../train_model_lightning.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
