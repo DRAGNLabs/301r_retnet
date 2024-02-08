322a43302497bd0331335ab70a3177e9fb4446f6
Nathan Prisbrey <ntpbanana@gmail.com>
2024-02-08 09:44:10 +0000

    Refactor PyTorch Lightning Dataset Preparation
    Adjust code to better match recommendations and styling given at
    "https://lightning.ai/docs/pytorch/stable/data/datamodule.html".

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from pytorch_lightning import LightningDataModule
-from typing import List, Optional
+from typing import Optional
 from torch.utils.data import DataLoader
 
 from datasets import load_dataset
@@ -11,27 +11,43 @@ class DataModule(LightningDataModule):
         self.batch_size = config.batch_size
         self.num_workers = num_workers
 
-        # Load tokenized datasets
-        self.train_tokenized_dataset = load_dataset(
-            "parquet",
-            data_files=str(Path(config.tokenized_dataset_path) / "train.parquet"),
-            split="all")
-        self.val_tokenized_dataset = load_dataset(
-            "parquet",
-            data_files=str(Path(config.tokenized_dataset_path) / "validation.parquet"),
-            split="all")
-        self.test_tokenized_dataset = load_dataset(
-            "parquet",
-            data_files=str(Path(config.tokenized_dataset_path) / "test.parquet"),
-            split="all")
-    
-    def setup(self, stage: Optional[str] = None):
-        self.train_dataset = self.train_tokenized_dataset.with_format("torch")["input_ids"]
-        self.val_dataset = self.val_tokenized_dataset.with_format("torch")["input_ids"]
-        self.test_dataset = self.test_tokenized_dataset.with_format("torch")["input_ids"]
+    def setup(self, stage: str):
+        if stage == "fit":
+            # Load datasets
+            train_tokenized_dataset = load_dataset(
+                "parquet",
+                data_files=str(
+                    Path(config.tokenized_dataset_path) / "train.parquet"),
+                split="all")
+            val_tokenized_dataset = load_dataset(
+                "parquet",
+                data_files=str(
+                    Path(config.tokenized_dataset_path) / "validation.parquet"),
+                split="all")
+
+            # Convert datasets into PyTorch format
+            self.train_dataset = \
+                train_tokenized_dataset.with_format("torch")["input_ids"]
+            self.val_dataset = \
+                val_tokenized_dataset.with_format("torch")["input_ids"]
+
+        if stage == "test":
+            # Load dataset
+            test_tokenized_dataset = load_dataset(
+                "parquet",
+                data_files=str(
+                    Path(config.tokenized_dataset_path) / "test.parquet"),
+                split="all")
+
+            # Convert datasets into PyTorch format
+            self.test_dataset = \
+                test_tokenized_dataset.with_format("torch")["input_ids"]
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True)
     
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
