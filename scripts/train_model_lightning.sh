745da717ac6c077873751caf22b208edc22d6d12
Nathan Prisbrey <ntpbanana@gmail.com>
2024-02-09 13:22:52 -0700

    Format And Make Executable Scripts

--- scripts/train_model_lightning.sh
+++ scripts/train_model_lightning.sh
@@ -1,1 +1,3 @@
-python3 ../../train_model_lightning.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
+python3 \
+    ../../train_model_lightning.py \
+    ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
