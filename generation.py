6ba1d1caa82b704d55d6ea5ab495b5786e9843ae
Nathan Prisbrey <ntpbanana@gmail.com>
2024-02-10 02:08:52 +0000

    Touch Up generation.py And Add gen_len
    Add new parameter to the configuration file to prevent hard-coding the
    generation length.

--- generation.py
+++ generation.py
@@ -2,42 +2,53 @@ import torch
 import sys
 import yaml
 
-from transformers import PreTrainedTokenizerFast
-
 from models import RetNetModel, TransformerModel
+from transformers import PreTrainedTokenizerFast
 from utils import Struct, generate_text
 
-def generate_specific_text(config):
-    # Generate text from the model
-    print("\nGenerating text...")
+def generate_specific_text(config: Struct):
+    """
+    Args:
+        config (Struct): A Struct object with all configuration fields.
+    """
     input_starting_strings = [
-        "<pad>",
-        "= valkyria",
-        "= = reception ="]
+        "Once upon a time, there was",
+        "A long time ago, in a galaxy far, far",
+        "It truly happened so suddenly! In one moment"]
 
+    # Create appropriate model type
     if config.model_type.lower() == "retnet":
         model = RetNetModel(config)
     elif config.model_type.lower() == "transformer":
         model = TransformerModel(config)
 
+    # Load in pre-trained weights from checkpoint
+    if config.checkpoint_path is None:
+        raise ValueError(
+            "To generate text, the 'checkpoint_path' value in the " + \
+            "configuration file must be set")
     checkpoint = torch.load(config.checkpoint_path)
-    model.load_state_dict(checkpoint['state_dict'])
+    model.load_state_dict(checkpoint["state_dict"])
 
+    # Load pre-trained tokenizer
     tokenizer = PreTrainedTokenizerFast.from_pretrained(config.tokenizer_path)
 
     device = torch.device(config.device)
 
+    # Get completed strings
+    print("\nGenerating text...")
     generated_strings = generate_text(
         model=model,
         tokenizer=tokenizer,
         start_string_list=input_starting_strings,
         device=device,
         seq_len=config.seq_len,
-        generation_length=100)
+        generation_length=config.gen_len)
 
     print("Generated strings:")
     for idx, string in enumerate(generated_strings):
         print(f"{idx+1}: {string}\n")
+
 
 if __name__ == "__main__":
     args = sys.argv
@@ -49,4 +60,3 @@ if __name__ == "__main__":
     config = Struct(**config)
 
     generate_specific_text(config)
-   
