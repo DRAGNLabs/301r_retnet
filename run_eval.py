9abcc23c6a2ef30030a41c3bc7f7119262d69ad3
Nathan Prisbrey <ntpbanana@gmail.com>
2024-02-10 03:41:01 +0000

    Touchups For run_eval.py

--- run_eval.py
+++ run_eval.py
@@ -11,46 +11,46 @@ from transformers import AutoModel, AutoConfig, AutoModelForCausalLM
 from typing import Optional, List
 from utils import Struct
 
-def run_eval(config):
+def run_eval(config: Struct):
+    """ Run evaluation on all tasks (benchmarks).
+    Args:
+        config (Struct): A Struct object with all configuration fields.
     """
-    Run evaluation on all tasks
-    :return: None
-    """
-
     if config.model_path_dir is None:
         raise ValueError("model_path_dir requires a path, but none was given.")
     if config.results_out_path is None:
-        raise ValueError("results_out_path requires a path, but none was given.")
-    
+        raise ValueError("results_out_path requires a path, but none was " + \
+            "given.")
+
     AutoConfig.register("retnet", RetNetConfig)
     AutoConfig.register("custom_transformer", DecoderConfig)
     AutoModel.register(RetNetConfig, RetNetModelHF)
     AutoModel.register(DecoderConfig, TransformerModelHF)
     AutoModelForCausalLM.register(RetNetConfig, RetNetModelHF)
     AutoModelForCausalLM.register(DecoderConfig, TransformerModelHF)
-    
+
     lm_eval.tasks.initialize_tasks()
     if config.tokenizer_path:
         results = lm_eval.simple_evaluate(
             model="hf",
-            model_args=f"pretrained={config.model_path_dir},tokenizer={config.tokenizer_path}",
+            model_args=f"pretrained={config.model_path_dir}," + \
+                f"tokenizer={config.tokenizer_path}",
             tasks=config.tasks,
             num_fewshot=0,
-            device=config.device,
-        )
+            device=config.device)
     else:
         results = lm_eval.simple_evaluate(
             model="hf",
             model_args=f"pretrained={config.model_path_dir}",
             tasks=config.tasks,
             num_fewshot=0,
-            device=config.device,
-        )
+            device=config.device)
 
     print(results["results"])
 
     with open(config.results_out_path, "w") as f:
         json.dump(results["results"], f, indent=4)
+
 
 if __name__ == "__main__":
     args = sys.argv
