b3640f2a4316084c7db06f7b929981124bd47621
Kimball Jardine <kimball.jardine@outlook.com>
2024-02-09 19:52:11 -0700

    Clean up new lines, ' --> " in run_eval.py

--- run_eval.py
+++ run_eval.py
@@ -10,9 +10,9 @@ from typing import Optional, List
 
 def run_eval(
         model_path_dir: str,
-        device: str = 'cuda:0',
+        device: str = "cuda:0",
         results_out_path: Optional[str] = None,
-        tasks: List[str] = ['hellaswag'],
+        tasks: List[str] = ["hellaswag"],
         tokenizer_path_dir: Optional[str] = None):
     """
     Run evaluation on all tasks
@@ -20,19 +20,17 @@ def run_eval(
     :param tokenizer_path_dir: path to tokenizer directory (Required only if tokenizer is not in model directory)
     :return: None
     """
-    AutoConfig.register('retnet', RetNetConfig)
-    AutoConfig.register('custom_transformer', DecoderConfig)
+    AutoConfig.register("retnet", RetNetConfig)
+    AutoConfig.register("custom_transformer", DecoderConfig)
     AutoModel.register(RetNetConfig, RetNetModelHF)
     AutoModel.register(DecoderConfig, TransformerModelHF)
     AutoModelForCausalLM.register(RetNetConfig, RetNetModelHF)
     AutoModelForCausalLM.register(DecoderConfig, TransformerModelHF)
 
-
-
     lm_eval.tasks.initialize_tasks()
     if tokenizer_path_dir:
         results = lm_eval.simple_evaluate(
-            model='hf',
+            model="hf",
             model_args=f"pretrained={model_path_dir},tokenizer={tokenizer_path_dir}",
             tasks=tasks,
             num_fewshot=0,
@@ -40,24 +38,23 @@ def run_eval(
         )
     else:
         results = lm_eval.simple_evaluate(
-            model='hf',
+            model="hf",
             model_args=f"pretrained={model_path_dir}",
             tasks=tasks,
             num_fewshot=0,
             device=device,
         )
 
-    print(results['results'])
+    print(results["results"])
     model_name = Path(model_path_dir).name
     if not results_out_path:
         print(f"No results path specified, saving to {model_name}_results.json")
         results_out_path = f"{model_name}_results.json"
 
-    with open(results_out_path, 'w') as f:
-        json.dump(results['results'], f, indent=4)
+    with open(results_out_path, "w") as f:
+        json.dump(results["results"], f, indent=4)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
     
     parser.add_argument('--device', type=str, default='cuda:0', help='Device to run on')
