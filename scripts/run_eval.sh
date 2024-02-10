7c86126743e4eb8879aface094d8d898f60bf802
Kimball Jardine <kimball.jardine@outlook.com>
2024-02-09 20:20:53 -0700

    More cleanup of yaml file, fix run_eval.sh files

---
+++ scripts/run_eval.sh
@@ -0,0 +1,1 @@
+python3 ../../run_eval.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
--- slurm/run_eval.sh
+++ slurm/run_eval.sh
@@ -13,16 +13,6 @@
 # ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
-# Make sure to set these
-MODEL_PATH_DIR=/tmp/models/retnet
-TOKENIZER_PATH_DIR=/grphome/grp_retnet/compute/tokenizers/wikitext
-TASKS=winogrande
-
-# BEFORE RUNNING THIS SCRIPT ON THE COMPUTE NODE: Make sure to run the
-# run_eval.py script locally to download and cache the correct datasets and
-# benchmarks. You do not need to let it run to completion, just until the
-# datasets and benchmarks are downloaded and cached.
-
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 mamba activate retnet
-python3 ../../run_eval.py --model-path-dir ${MODEL_PATH_DIR} --tokenizer-path-dir ${TOKENIZER_PATH_DIR} --tasks ${TASKS}
+python3 ../../run_eval.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
