edb7cc480c89a5ce83429f513b8a9e76c47af970
Drew Galbraith <drew.s.galbraith@gmail.com>
2024-02-12 11:41:36 -0700

    change 'mamba activate retnet' to 'mamba activate <your_env_name>'

--- slurm/train_tokenizer.sh
+++ slurm/train_tokenizer.sh
@@ -12,7 +12,7 @@
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-mamba activate retnet
+mamba activate <your_env_name>
 python3 \
     ../../train_tokenizer.py \
     ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
