28660b911bef2ddc609b56455ea5d59a3bbff2e7
Drew Galbraith <drew.s.galbraith@gmail.com>
2024-02-12 11:51:34 -0700

    capitalize mamba name to <YOUR_ENV_NAME>

--- slurm/generate.sh
+++ slurm/generate.sh
@@ -14,5 +14,5 @@
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-mamba activate <your_env_name>
+mamba activate <YOUR_ENV_NAME>
 python3 ../../generate.py ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml
