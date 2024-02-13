cba8fe1a2a5dfdbc037bc6c9807f4fea0a483e73
Nathan Prisbrey <ntpbanana@gmail.com>
2024-02-13 23:36:25 +0000

    Update Script Explicit Parameters

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -1,1 +1,1 @@
-python3 ../../grid_search.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
+python3 ../../grid_search.py ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml
--- scripts/tokenize_data.sh
+++ scripts/tokenize_data.sh
@@ -1,1 +1,3 @@
-python3 ../../tokenize_data.py ../../configs/user_configs/YOUR_CONFIG_HERE.yaml
+python3 \
+    ../../tokenize_data.py \
+    ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml
