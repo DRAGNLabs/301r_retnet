3bba39625942c910d8fc87f9d29e62a3a5f53e85
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-02-28 13:31:34 -0700

    Rename files

---
+++ slurm/gpt4_eval_child.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --login
+
+#SBATCH --time=1:00:00   # walltime
+#SBATCH --ntasks-per-node=1# number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:1
+#SBATCH --qos=cs
+#SBATCH --mem=64G   # memory per CPU core
+#SBATCH -J "generate"   # job name
+#SBATCH --output=%x_%j.out
+
+# Set the max number of threads to use for programs using OpenMP. Should be <=
+# ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+mamba activate <YOUR_ENV_NAME>
+
+## NOT TO RUN INDEPENDENT; CALL BY PARENT SCRIPT ##
+
+# Model 1
+python3 ../../generate.py ../../configs/user_configs/config_benchmarks_retnet.yaml
+echo "\nOne Mississippi..."
+
+# Model 2
+python3 ../../generate.py ../../configs/user_configs/config_benchmarks_transformer.yaml
+echo "\nTwo Mississippi..."
+
+# # Model n
+# python3 ...
+# echo "\n Mississippi..."
+
+# Signal that the job has finished
+rm red_light.txt
---
+++ slurm/gpt4_eval_parent.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+/home/dsg2060/.lmod.d
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+mamba activate <YOUR_ENV_NAME>
+
+## REQUIRES CHILD SCRIPT OR WILL LOOP INDEFINITELY ##
+
+# Create a text file
+echo "This is an dummy file. We will delete it to signal when inference finishes." > red_light.txt
+
+# submit inference job
+sbatch <YOUR_gpt4_eval_child.sh_SCRIPT> 
+
+# Check if the dummy script exists; loop until it doesn't.
+while true; do
+  if [ -f red_light.txt ]; then
+    sleep 5
+  else
+    echo "Green light!\n"
+    break
+  fi
+done
+
+# execute python script; do not change python file path/content
+python3 /grphome/grp_retnet/compute/evaluation_metrics/gpt4_eval.py ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml
