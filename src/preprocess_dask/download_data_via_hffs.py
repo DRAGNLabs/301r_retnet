70aae9ec6a213274350296e9d819a1b7c05cb6b8
JayOrten <jay.orten@gmail.com>
2024-03-08 16:48:49 -0700

    clean up hffs download

--- src/preprocess_dask/download_data_via_hffs.py
+++ src/preprocess_dask/download_data_via_hffs.py
@@ -1,24 +1,22 @@
-import datasets
 import sys
 import yaml
 
-from datasets import DatasetDict
 from pathlib import Path
 from utils import Struct
 
 import dask
 dask.config.set({'dataframe.query-planning': True})
 import dask.dataframe as dd
-import dask_ml
 
 from huggingface_hub import login
 
 def download_data(config):
     """ 
-    Download dataset from Hugging Face through the HuggingFace file system, and save as parquet.
+    Download dataset from Hugging Face through the 
+    HuggingFace file system, and save as parquet.
 
-    There are a variety of ways to download datasets from HuggingFace,
-    and the best way depends on the dataset.
+    There are a variety of ways to download datasets 
+    from HuggingFace, and the best way depends on the dataset.
 
     This function uses the HuggingFace file system to download the data directly into
     Dask dataframes, which then save the data to disk in parquet format.
@@ -37,13 +35,15 @@ def download_data(config):
     print(f"Data name: {config.dataset_name}")
     print(f"Data subset: {config.dataset_subset}")
 
+    # This path may need to be modified depending on the dataset.
+    fs_path = f"hf://datasets/{config.dataset_name}/{config.dataset_subset}"
+
     # Can also use 'read_json', etc.; depends on HF repo.
-    dataset = dd.read_parquet("hf://datasets/wikitext/wikitext-2-v1")
+    dataset = dd.read_parquet(fs_path)
 
     print('Saving to disk')
     dataset.to_parquet(dataset_dir)
     print("Download completed.")
-
 
 if __name__ == "__main__":
     args = sys.argv
