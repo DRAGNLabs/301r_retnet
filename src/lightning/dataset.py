ca154da8c65970882c08e630f1255cec21c0ddff
JayOrten <jay.orten@gmail.com>
2024-03-09 12:45:45 -0700

    clean up dataset class

--- src/lightning/dataset.py
+++ src/lightning/dataset.py
@@ -1,20 +1,17 @@
-import os
-from datasets import load_dataset
+import dask
+import dask.dataframe as dd
+import torch
+import psutil
+import sys
+
 from pathlib import Path
 from pytorch_lightning import LightningDataModule
-import torch
-from torch.utils.data import DataLoader
-from utils import Struct
-import psutil
-
-import dask
-dask.config.set({'dataframe.query-planning': True})
-import dask.dataframe as dd
-
-from torch.utils.data import get_worker_info
+from torch.utils.data import DataLoader, get_worker_info
 from torch.distributed import get_rank, get_world_size
+from utils import Struct
 
-from itertools import (takewhile,repeat)
+dask.config.set({'dataframe.query-planning': True})
+sys.path.append("..")
 
 class DataModule(LightningDataModule):
     """
@@ -32,17 +29,6 @@ class DataModule(LightningDataModule):
         self.num_workers = config.num_workers
         self.tokenized_dataset_path = Path(config.tokenized_dataset_path)
         self.seq_len = config.seq_len
-
-        # print('Loading datasets...')
-        # self.train_dataset = DataSet(self.tokenized_dataset_path / 'train',
-        #                                  self.seq_len)
-            
-        # self.val_dataset = DataSet(self.tokenized_dataset_path / 'validation',
-        #                                 self.seq_len)
-        
-        # self.test_dataset = DataSet(self.tokenized_dataset_path / 'test',
-        #                                  self.seq_len)
-        # print('Datasets loaded.')
 
     def setup(self, stage: str):
         """ Setup for each stage -- called on every process on DDP.
@@ -86,31 +72,6 @@ class DataModule(LightningDataModule):
             dataset=self.test_dataset,
             batch_size=self.batch_size,
             num_workers=self.num_workers)
-
-# class DataSet(torch.utils.data.Dataset):
-#     def __init__(self, path_to_data, seq_len):
-#         # Read data with Dask, then compute into Pandas DF
-#         #self.data = dd.read_parquet(path_to_data / "*.parquet").compute()
-#         self.data = dd.read_parquet(path_to_data / "*.parquet").iterrows()
-#         #print(self.data.head())
-#         print('memory usage 3: ', psutil.virtual_memory().percent)
-#         self.seq_len = seq_len
-
-#     def __len__(self):
-#         return len(self.data)
-    
-#     def __getitem__(self, index):
-#         row = self.data.iloc[index,:].iloc[0]# Returns a pd.Series
-#         #print('row:',row)
-#         #print(type(row))
-
-#         x = row[:self.seq_len]
-#         y_true = row[1:self.seq_len+1]
-
-#         #print('x:', x)
-#         #print('y_true:', y_true)
-
-#         return x, y_true
     
 class DataSet(torch.utils.data.IterableDataset):
     def __init__(self, path_to_data, seq_len):
@@ -120,8 +81,6 @@ class DataSet(torch.utils.data.IterableDataset):
         # Get length of df
         self.length = len(self.data)
 
-        print('length: ', self.length)
-        print('memory usage 3: ', psutil.virtual_memory().percent)
         self.data = self.data.iterrows()
         self.seq_len = seq_len
 
@@ -143,11 +102,6 @@ class DataSet(torch.utils.data.IterableDataset):
         for index, item in enumerate(self.data):
             if index % (num_workers * world_size) == (process_rank * num_workers + worker_id):
                 item = item[1].values[0].copy()
-                #print('seq len: ', self.seq_len)
-                #print('item: ', item)
-                #print('len item: ', len(item))
                 x = item[:self.seq_len-1]
                 y_true = item[1:self.seq_len]
-                #print('x: ', x)
-                #print('y: ', y_true)
                 yield(x,y_true)
