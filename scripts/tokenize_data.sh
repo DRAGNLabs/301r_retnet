278db08d0923efa6455abe9a46f4740381c53f4b
JayOrten <jay.orten@gmail.com>
2024-03-09 15:34:07 -0700

    fix argument parsing

--- scripts/tokenize_data.sh
+++ scripts/tokenize_data.sh
@@ -1,3 +1,4 @@
 python3 \
     ../../src/tokenize_data.py \
-    ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml
+    ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml \
+    <YOUR_SPLIT_HERE> # train, validation, or test. You can start a job for each split.
