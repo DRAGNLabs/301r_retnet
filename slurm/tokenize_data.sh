9e97b98f04d8f1d055bfdd0b01bfc8d06a96aa66
Drew Galbraith <drew.s.galbraith@gmail.com>
2024-03-13 15:22:26 -0600

    Indicate datatype in comment

--- slurm/tokenize_data.sh
+++ slurm/tokenize_data.sh
@@ -16,4 +16,4 @@ mamba activate <YOUR_ENV_NAME>
 python3 \
     ../../src/tokenize_data.py \
     ../../configs/user_configs/<YOUR_CONFIG_HERE>.yaml \
-    <YOUR_SPLIT_HERE> # train, validation, or test. You can start a job for each split.
+    <YOUR_SPLIT_HERE> # 'train', 'validation', or 'test'. You can start a job for each split.
