88d337eae84081679f5055454bd7988ad5db438d
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-03-30 18:09:23 -0600

    fix warning strings

--- src/train_model.py
+++ src/train_model.py
@@ -27,12 +27,12 @@ class CustomModelCheckpoint(ModelCheckpoint):
         if every_n_hours is not None and every_n_train_steps is not None:
             if every_n_hours <= 0:
                 print("Warning: You have set both 'every_n_hours' and 'every_n_train_steps' in your yaml.")
-                print(f"With 'every_n_hours' set {every_n_hours}, i.e., <= 0; 
+                print(f"With 'every_n_hours' set {every_n_hours}, i.e., <= 0; \
                       resetting to 'None' and using every_n_train_steps ({every_n_train_steps}).")
                 every_n_hours = None
             else:
                 print("Warning: You have set both 'every_n_hours' and 'every_n_train_steps' in your yaml.")
-                print(f"Using 'every_n_hours' ({every_n_hours}) and changing 'every_n_train_steps' from 
+                print(f"Using 'every_n_hours' ({every_n_hours}) and changing 'every_n_train_steps' from \
                       {every_n_train_steps} to 'None'.")
                 every_n_train_steps = None
 
