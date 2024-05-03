19833812daf91a7bddc3266ab245582097b35638
DrewGalbraith <drew.s.galbraith@gmail.com>
2024-05-03 16:53:54 -0600

    Add comments, print statements, and fix eternal loops

--- src/generate_math.py
+++ src/generate_math.py
@@ -60,6 +60,7 @@ def generate(config: Struct):
         device=device,
         seq_len=config.seq_len,
         generation_length=config.gen_len)
+    
 
     print("Generated strings:")
     for idx, string in enumerate(generated_strings):
@@ -67,10 +68,12 @@ def generate(config: Struct):
 
     if config.csv_path is not None:
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        with open(config.csv_path, 'a', newline='') as outf:
+        with open(config.csv_path, 'w', newline='') as outf:
             writer = csv.writer(outf)
-            for string in generated_strings:
-                writer.writerow([current_time, config.model_type, string])
+            for pred in generated_strings:
+                y, y_hat = pred
+                pred_result = True if y in y_hat else False  # See if the target is in the prediction
+                writer.writerow([current_time, config.model_type, y_hat, y, pred_result])
 
 def generate_text(
         model: nn.Module,
@@ -93,36 +96,33 @@ def generate_text(
     Returns:
         A list of all the fully generated strings by the model.
     """
-    n_prompts = 1
+    n_prompts = 100
     n_shot = 6
 
     # Keep track of fully generated token indices lists
     generated_token_idx_list = []
 
     # Create list of strings from all lines in start_string_list path
-    df = pd.read_csv(generation_path, names=["data"], nrows=10)
+    df = pd.read_csv(generation_path, names=["data"], nrows=1000)
 
     prompts = df.data.tolist()
     # Randomly pick n_prompts lines from prompts
     prompts = random.sample(prompts, n_prompts * n_shot)
 
-    # Split prompts into n_shot lists of n_prompts
-    prompts = [prompts[i:i+n_shot] for i in range(0, len(prompts), n_shot)]
-
+    # Split prompts into n_prompt lists of len n_shot
+    prompts = [prompts[i:i+n_shot] for i in range(0, len(prompts), n_shot)]  # split prompts into list of N-shot-length sub lists
+    
     full_prompts = []
     full_prompt_targets = []
     # Append each prompt list together into a main prompt list
     for prompt_list in prompts:
-        n_shots = prompt_list[:n_shot-1]
-        final_prompt = prompt_list[-1]
-        final_prompt_source = final_prompt[:final_prompt.index("=")+1]
+        n_shots = prompt_list[:n_shot-1]  # Nshot examples
+        final_prompt = prompt_list[-1]  # test examples
+        final_prompt_source = final_prompt[:final_prompt.index("=")+1]  # cut answer out of final example
         # Join together into single string
         full_prompt = "\n".join(n_shots) + "\n" + final_prompt_source
         full_prompt_targets.append("\n".join(prompt_list))
         full_prompts.append(full_prompt)
-
-    print('full_prompts:', full_prompts)
-    print('full_prompt_targets:', full_prompt_targets)
 
     # Convert initial start strings into tokenized sequences
     tokenized_start_list = tokenizer(
@@ -142,6 +142,7 @@ def generate_text(
         for input_idx in range(len(tokenized_start_list)):
             # Retrieve string's tokenized version
             input_token_idxs = tokenized_start_list[input_idx]
+            len_input = len(input_token_idxs)
 
             # Store generated sequence token indices
             generated_token_idxs = input_token_idxs
@@ -151,20 +152,23 @@ def generate_text(
 
             # Add batch dimension and move to device
             input_tensor = input_tensor.unsqueeze(0).to(device)
+            print(f"\n\nStarting on item {input_idx}")
 
             # Keep generating until padding or reached generation length
             while generated_token_idxs[-1] != tokenizer.pad_token_id \
-                    and len(generated_token_idxs) < generation_length:
+                    and len(generated_token_idxs) < generation_length+len_input:
+                
+                print(f"Generated {len(generated_token_idxs)} of {len_input+generation_length} tokens (includes prompt).")
                 # Make sure input_tensor isn't longer than sequence length
-                input_tensor = input_tensor[
-                    :,
-                    max(0, input_tensor.shape[-1] - seq_len):]
+                input_tensor = input_tensor[: , max(0, input_tensor.shape[-1] - seq_len):]
 
                 # Get model predictions
                 predictions = model(input_tensor)
 
                 # Apply softmax to predictions
                 predictions = torch.nn.functional.softmax(predictions, dim=-1)
+                # print('\nSoftmaxed predictions:', predictions)
+
 
                 # Get the last predicted word
                 predicted_id = predictions.argmax(dim=-1)[0, -1]
@@ -173,17 +177,20 @@ def generate_text(
                 input_tensor = torch.cat(
                     [input_tensor, predicted_id[None, None]],
                     dim=-1)
+                
+                print(generated_token_idxs)
 
                 # Store predicted token as part of generation
                 generated_token_idxs.append(predicted_id.item())
+                print(generated_token_idxs)
 
             # Store fully generated sequence of token indices for start string
             generated_token_idx_list.append(generated_token_idxs)
 
     # Decode token indices lists to lists of strings and return
     decoded_predictions = tokenizer.batch_decode(generated_token_idx_list)
-
-    return decoded_predictions
+    
+    return zip(decoded_predictions, full_prompt_targets)
 
 if __name__ == "__main__":
     print('Starting')
